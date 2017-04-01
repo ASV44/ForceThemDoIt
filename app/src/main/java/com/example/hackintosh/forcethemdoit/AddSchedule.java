573686269ce97b1bbf14265f896076c786464626
strdr4605 <str.dr4605@gmail.com>
2017-04-02 00:58:17 +0300

    recipients

--- app/src/main/java/com/example/hackintosh/forcethemdoit/AddSchedule.java
+++ app/src/main/java/com/example/hackintosh/forcethemdoit/AddSchedule.java
@@ -6,8 +6,13 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.List;
+import java.util.Map;
 
 public class AddSchedule extends AppCompatActivity {
 
@@ -19,10 +24,13 @@ public class AddSchedule extends AppCompatActivity {
         setContentView(R.layout.activity_add_schedule);
 
         Button sendSms = (Button) findViewById(R.id.getCsvText);
+        final TextView recipientsList = (TextView) findViewById(R.id.recipientsList);
 
         sendSms.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String recipientsListString = recipientsList.getText().toString();
+                Log.d("myTag2", recipientsListString);
 
                 sender.sendSms("068591082", "We are developing SMS sender now");
             }
@@ -34,4 +42,9 @@ public class AddSchedule extends AppCompatActivity {
             ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.SEND_SMS},1);
         }
     }
+
+//    private Map<String, String> recipientsMap(String recipientsListString) {
+//        String[] lines = recipientsListString.split( "\n" );
+//        return 0;
+//    }
 }
